51b24a3d2164d7da2a785cb92bbcedc4ec1bc43c
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 14:14:07 -0600

    axios changes

--- public/index.js
+++ public/index.js
@@ -1,15 +1,15 @@
 
 //const baseUrl = 'http://localhost:5050/'
-const baseUrl = 'https://traceability-lab21.herokuapp.com'
+//const baseUrl = 'https://traceability-lab21.herokuapp.com'
 
 
-const pickColor = () => {
-    const body = {
-        color: 'white'
-    }
-    axios.post(baseUrl + 'color', body).then(res => {
-        console.log(res.data)
-    })
-}
+// const pickColor = () => {
+//     const body = {
+//         color: 'white'
+//     }
+//     axios.post(baseUrl + 'color', body).then(res => {
+//         console.log(res.data)
+//     })
+// }
 
-document.querySelector('.color-button').addEventListener('click', pickColor)
+// document.querySelector('.color-button').addEventListener('click', pickColor)
