f2f7c7fce0b2ff129dab59aa70bd3e354f88be96
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 11:21:24 -0600

    rollbar

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 5050
 
-// const Rollbar = require("rollbar")
-// const rollbar = newRollbar({
+const Rollbar = require("rollbar")
+var rollbar = new Rollbar({
+    accessToken: 'cb3f3025dfe644e9b5d26d2d6b864677',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 
-// })
+  // record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
