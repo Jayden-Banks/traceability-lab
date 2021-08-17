eb6e7782e773e770886cc0cafdb2c346ee9aac53
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 12:10:55 -0600

    html

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 5050
 
-const Rollbar = require("rollbar")
-var rollbar = new Rollbar({
-    accessToken: 'cb3f3025dfe644e9b5d26d2d6b864677',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-});
+
+// const Rollbar = require("rollbar")
+// var rollbar = new Rollbar({
+//     accessToken: 'cb3f3025dfe644e9b5d26d2d6b864677',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// });
 
   // record a generic message and send it to Rollbar
-
+app.use(express.static('public'))
 app.use(express.json())
 
 
@@ -21,8 +22,8 @@ app.use(express.json())
 
 //End points
 app.get('/', (req, res) => {
-    rollbar.log("Hello world!");
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    //rollbar.log("Hello world!");
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 //takes a color in and sends it back 
 app.post('/color', (req, res) => {
