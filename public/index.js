095c55ad2fc1702c29a2832fa4a9dd1db9e877d2
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 14:19:38 -0600

    does this still work?

--- public/index.js
+++ public/index.js
@@ -1,15 +1,17 @@
+//const { default: axios } = require("axios")
+
 
 //const baseUrl = 'http://localhost:5050/'
-//const baseUrl = 'https://traceability-lab21.herokuapp.com'
 
 
 // const pickColor = () => {
 //     const body = {
 //         color: 'white'
 //     }
-//     axios.post(baseUrl + 'color', body).then(res => {
+//     axios.post('/color', body).then(res => {
 //         console.log(res.data)
-//     })
+//         document.querySelector('h2').textContent = res.data
+// })
 // }
 
 // document.querySelector('.color-button').addEventListener('click', pickColor)
