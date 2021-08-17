d608ea6b8a8a1fac201a3b06edd85e5160c6c282
Jayden Banks <jayden.banks21@gmail.com>
2021-08-17 13:24:41 -0600

    url

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 
-const baseUrl = 'http://localhost:5050/'
-// const baseUrl = 'https://traceability-lab21.herokuapp.com'
+//const baseUrl = 'http://localhost:5050/'
+const baseUrl = 'https://traceability-lab21.herokuapp.com'
 
 
 const pickColor = () => {
